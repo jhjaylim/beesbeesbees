0f081e715369ff6d2463b047f9bd5a7f83e09dbd
jhjaylim <jhjaylim@gmail.com>
2017-07-10 15:57:08 -0700

    Finish BeesBeesBees

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,17 @@
 var Grub = function() {
-};
 
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
+
+};
+//an age property that is set to 0
+//a color property that is set to pink
+//a food property that is set to jelly
+//an eat method
+
+Grub.prototype.eat = function() {
+
+};
+// Grub.prototype.food = 'jelly'; 
